b2e1f67f969f581a515be212edf93eac8ae6fe5b
liyunhe <liyunheasap@yeah.net>
2022-10-01 13:45:11 +0800

    composer ext-mbstring

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9d382cb60d98b3782465dff72206a81d37c59fa2',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9d382cb60d98b3782465dff72206a81d37c59fa2',
+            'dev_requirement' => false,
+        ),
+    ),
+);
